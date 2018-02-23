3b120de70ee5916e0b36027ebaf59cea326d5ab7
KB9 <kavanbickerstaff@googlemail.com>
2018-02-23 19:11:55 +0000

    apt-get installs allowed by default

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # OS libraries
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm
-sudo apt-get install git gfortran
-sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm -y
+sudo apt-get install git gfortran -y
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev -y
  
-sudo apt-get install libtiff5-dev
+sudo apt-get install libtiff5-dev -y
  
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev -y
+sudo apt-get install libxine2-dev libv4l-dev -y
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev -y
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev -y
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev -y
+sudo apt-get install libvorbis-dev libxvidcore-dev -y
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev -y
+sudo apt-get install x264 v4l-utils -y
 
 # Clone OpenCV contrib 3.3.1 if it is not already present
 if [ ! -d "opencv_contrib" ]; then
